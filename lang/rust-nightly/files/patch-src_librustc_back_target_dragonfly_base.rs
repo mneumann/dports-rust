b7872e5bbcf19d5ae15fbc8802da187e7d43d1af
Michael Neumann <mneumann@ntecs.de>
2015-08-31 12:07:17 +0200

    New snapshot

--- lang/rust-nightly/files/patch-src_librustc_back_target_dragonfly_base.rs
+++ lang/rust-nightly/files/patch-src_librustc_back_target_dragonfly_base.rs
@@ -1,12 +1,11 @@
---- src/librustc_back/target/dragonfly_base.rs.orig
-+++ src/librustc_back/target/dragonfly_base.rs
-@@ -28,7 +28,7 @@ pub fn opts() -> TargetOptions {
+--- src/librustc_back/target/dragonfly_base.rs.orig	2015-07-26 12:39:18.000000000 +0200
++++ src/librustc_back/target/dragonfly_base.rs	2015-08-31 09:18:43.649340000 +0200
+@@ -29,7 +29,7 @@
              "-Wl,--as-needed".to_string(),
          ),
          position_independent_executables: true,
 -        archive_format: "bsd".to_string(),
 +        archive_format: "gnu".to_string(),
-         exe_allocation_crate: super::best_allocator(),
          .. Default::default()
      }
-
+ }
