eba0a885570b5be286b16c0b9d94e4f3a6a8dfce
Michael Neumann <mneumann@ntecs.de>
2015-09-02 00:52:35 +0200

    Fix patch

--- lang/rust-nightly/files/patch-src_librustc_back_target_dragonfly_base.rs
+++ lang/rust-nightly/files/patch-src_librustc_back_target_dragonfly_base.rs
@@ -1,12 +1,11 @@
---- src/librustc_back/target/dragonfly_base.rs.orig
-+++ src/librustc_back/target/dragonfly_base.rs
-@@ -28,7 +28,7 @@ pub fn opts() -> TargetOptions {
+--- src/librustc_back/target/dragonfly_base.rs.orig	2015-07-17 18:18:52.000000000 +0200
++++ src/librustc_back/target/dragonfly_base.rs	2015-09-01 23:00:57.937410000 +0200
+@@ -29,7 +29,7 @@
              "-Wl,--as-needed".to_string(),
          ),
          position_independent_executables: true,
 -        archive_format: "bsd".to_string(),
 +        archive_format: "gnu".to_string(),
-         exe_allocation_crate: super::best_allocator(),
          .. Default::default()
      }
-
+ }
